43c400c49e17c513d95616a078640a4393fb0055
mhss <mhss@cin.ufpe.br>
2017-04-22 00:24:49 -0300

    launch script

---
+++ SOM/launch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+nohup ./dim1_batch.sh &> dim1/out &
+nohup ./dim2_batch.sh &> dim2/out &
+nohup ./noise_batch.sh &> noise/out &
+nohup ./real_batch.sh &> real/out &
+nohup ./size1_batch.sh &> size1/out &
+nohup ./size2_batch.sh &> size2/out &
