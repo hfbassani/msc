4c3dc079339f0c88d02341503390b9ba26cdbe3a
mhss <mhss@cin.ufpe.br>
2017-04-22 00:37:21 -0300

    fixing launch script

--- SOM/launch.sh
+++ SOM/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-nohup ./dim1_batch.sh &> dim1/out &;
-nohup ./dim2_batch.sh &> dim2/out &;
-nohup ./noise_batch.sh &> noise/out &;
-nohup ./real_batch.sh &> real/out &;
-nohup ./size1_batch.sh &> size1/out &;
-nohup ./size2_batch.sh &> size2/out &;
+nohup ./dim1_batch.sh &> dim1_out &
+nohup ./dim2_batch.sh &> dim2_out &
+nohup ./noise_batch.sh &> noise_out &
+nohup ./real_batch.sh &> real_out &
+nohup ./size1_batch.sh &> size1_out &
+nohup ./size2_batch.sh &> size2_out &
