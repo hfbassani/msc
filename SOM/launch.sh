b028a06a4593450ba2bea941e5ae407e3ae1fef0
mhss <mhss@cin.ufpe.br>
2017-05-30 12:15:57 -0300

    messing with the scripts

--- SOM/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-nohup ./dim1_batch.sh &> dim1_out &
-nohup ./dim2_batch.sh &> dim2_out &
-nohup ./noise_batch.sh &> noise_out &
-nohup ./real_batch.sh &> real_out &
-nohup ./size1_batch.sh &> size1_out &
-nohup ./size2_batch.sh &> size2_out &
