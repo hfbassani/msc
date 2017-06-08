4bb5a245520ab0b68b5797d53ece6a613a08afdc
mhss <mhss@cin.ufpe.br>
2017-06-08 02:33:11 -0300

    balancing scripts

--- script.sh
+++ script.sh
@@ -5,4 +5,3 @@ nohup ./s0.sh &> ../error/err_0 &
 nohup ./s1.sh &> ../error/err_1 &
 nohup ./s2.sh &> ../error/err_2 &
 nohup ./s3.sh &> ../error/err_3 &
-nohup ./s4.sh &> ../error/err_4 &
--- script2.sh
+++ script2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir ../output
 mkdir ../error
+nohup ./s4.sh &> ../error/err_4 &
 nohup ./s5.sh &> ../error/err_5 &
 nohup ./s6.sh &> ../error/err_6 &
 nohup ./s7.sh &> ../error/err_7 &
