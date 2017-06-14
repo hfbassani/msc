e8f1759cb65d550471cd9b2d965afc8602808be3
mhss <mhss@cin.ufpe.br>
2017-06-14 20:48:58 -0300

    preparing to run second test

--- s_1.sh
+++ s_1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-mkdir ../lua_output
-mkdir ../lua_output/results
-mkdir ../lua_output/tmp
-time python s_1.py "luajit larfdssom_runner.lua" 1 "../lua_output"
-#mkdir ../c_output
-#mkdir ../c_output/results
-#mkdir ../c_output/tmp
-#time python s_1.py "../larfdssom" 1 "../c_output"
+#mkdir ../lua_output
+#mkdir ../lua_output/results
+#mkdir ../lua_output/tmp
+#time python s_1.py "luajit larfdssom_runner.lua" 1 "../lua_output"
+mkdir ../c_output
+mkdir ../c_output/results
+mkdir ../c_output/tmp
+time python s_1.py "../larfdssom" 1 "../c_output"
