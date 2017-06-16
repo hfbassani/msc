4ef9456f699a122c5b4988c3a6843dfdd7699bd3
mhss <mhss@cin.ufpe.br>
2017-06-15 21:34:56 -0300

    preparing scripts again

--- s_1.py
+++ s_1.py
@@ -7,12 +7,13 @@ if __name__ == '__main__':
 		'N10', 'N30', 'N50', 'N70',
 		'S1500', 'S2500', 'S3500', 'S4500', 'S5500'
 	]
-	files = [ 'D05', 'D25', 'D75', 'N10', 'N70', 'S1500', 'S5500' ]
 	input_folder = '../dbs'
 	params_folder = '../best_params'
 	files = [[f, input_folder, params_folder + '/' + f, False]
 			for f in files]
 	#for i in range(7):
 	#	files[i][3] = True
+
+	files = files[7:]
 	run_files(files)
 
