33b9d70fa2330f4ce6ff38afe5e2b66add4fbb0d
mhss <mhss@cin.ufpe.br>
2017-06-16 11:16:50 -0300

    preparing scripts

--- s_1.sh
+++ s_1.sh
@@ -3,7 +3,7 @@ mkdir ../lua_output
 mkdir ../lua_output/results
 mkdir ../lua_output/tmp
 time python s_1.py "th larfdssom_runner.lua" 1 "../lua_output"
-#mkdir ../c_output
-#mkdir ../c_output/results
-#mkdir ../c_output/tmp
-#time python s_1.py "../larfdssom" 30 "../c_output"
+mkdir ../c_output
+mkdir ../c_output/results
+mkdir ../c_output/tmp
+time python s_1.py "../larfdssom" 1 "../c_output"
