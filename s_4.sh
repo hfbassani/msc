b2df4ef1267b1fab789f3db936695abc91e411cb
mhss <mhss@cin.ufpe.br>
2017-06-22 13:47:29 -0300

    fixing scriptys

--- s_4.sh
+++ s_4.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+langs="c lua";
+for i in $langs;
+do
+  mkdir "$2/$i";
+  mkdir "$2/$i/results";
+  mkdir "$2/$i/tmp";
+done
 for i in $1;
 do
-  time python s_4.py "../larfdssom" 30 $2 $i;
-  time python s_4.py "th larfdssom_runner.lua" 30 $2 $i;
+  time python s_4.py "../larfdssom" 30 "$2/c" $i;
+  time python s_4.py "th larfdssom_runner.lua" 30 "$2/lua" $i;
 done
--- script4.sh
+++ script4.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-mkdir ../clus_output
-mkdir ../clus_output/results
-mkdir ../clus_output/tmp
-mkdir ../clus_output/error
-nohup ./s_4.sh "C008 C016 C032 C064 C128" "../clus_output" &> ../clus_output/error/err &
+folder="../clus_output"
+mkdir $folder
+nohup ./s_4.sh "C008 C016 C032 C064 C128" $folder &> "$folder/error" &
