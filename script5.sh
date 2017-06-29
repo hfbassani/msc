a155c99a889f7ba4dd8bac3bbb65fdd749ace156
mhss <mhss@cin.ufpe.br>
2017-06-28 21:50:36 -0300

    expanding script 5

--- script5.sh
+++ script5.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 folder="../dim_output"
 mkdir $folder
-nohup ./s_4.sh "D0032 D0064 D0128 D0256 D0512 D1024 D2048 D4096" $folder &> "$folder/error" &
+nohup ./s_4.sh "D0032 D0064 D0128 D0256 D0512 D1024 D2048 D4096 D8192 D16384 D32768" $folder &> "$folder/error" &
